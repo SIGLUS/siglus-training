d8ab2bb8146df2a1cf0c1ce3f4967801a6b4de83
yinshangwei <swyin@thoughtworks.com>
2023-03-23 17:06:49 +0800

    [shangwei] #N/A build local training server

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# siglus-training
+# Build local training server
+
+### Install
+```shell
+# git clone repository
+git clone https://github.com/SIGLUS/siglus-training.git
+
+# goto the folder siglus-training
+cd siglus-training
+
+# update settings.env file, ask TW to support, it's for security reason
+
+# connect internet and pull docker images (this may take a long time depend on your network condition)
+docker-compose pull
+
+# start docker containers
+docker-compose up -d
+
+# wait for about 10 minutes until all docker containers ready
+
+# restore uat DB dump file, ask TW to get the file
+./restore-db.sh <the absolute path of the uat DB dump file>
+
+# wait for about 10 minutes until DB file restore successfully
+
+# goto web portal
+open http://localhost
+```
+
+### Reinstall
+```shell
+# if your machine shutdown or restart, you can restart local training server
+# stop first
+docker-compose down
+
+# then start
+docker-compose up -d
+
+# wait for a few minutes
+
+# goto web portal
+open http://localhost
+```
---
+++ restore-db.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+dump_path=$1
+if [ -z "${dump_path}" ]; then
+   echo "dump_path is required"
+   exit 1
+fi
+pgpassword='p@ssw0rd'
+db_dns=localhost
+
+echo "clean db ${db_dns}, make sure siglusapi is down before restoring"
+PGPASSWORD="${pgpassword}" psql -h "${db_dns}" -U postgres open_lmis -c "drop schema if exists auth cascade;drop schema if exists fulfillment cascade;drop schema if exists notification cascade;drop schema if exists referencedata cascade;drop schema if exists report cascade;drop schema if exists requisition cascade;drop schema if exists siglusintegration cascade;drop schema if exists stockmanagement cascade;drop schema if exists dashboard cascade;drop schema if exists datamigration cascade;drop schema if exists localmachine cascade;"
+PGPASSWORD="${pgpassword}" psql -h "${db_dns}" -U postgres open_lmis -c "drop publication if exists dbz_publication;select pg_drop_replication_slot('debezium');" || echo "fail to drop debezium slot"
+
+echo "restore open_lmis"
+gunzip -c "${dump_path}" | PGPASSWORD="${pgpassword}" psql -h "${db_dns}" -U postgres open_lmis
+
+echo "delete cdc_offset_backing"
+PGPASSWORD="${pgpassword}" psql -h "${db_dns}" -U postgres open_lmis -c "drop publication if exists dbz_publication;DELETE FROM localmachine.cdc_offset_backing;" || echo "fail to reset publication and offset backing"
+
+echo "restore done, need to clean cache"
